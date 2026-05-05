3f5a1a0ce1695d9bb1e5101eed11245537db0d0a
andretfw <118443112+andretfw@users.noreply.github.com>
2026-05-06 01:02:32 +0300

    Update ArtSection.tsx

--- src/components/ArtSection.tsx
+++ src/components/ArtSection.tsx
@@ -51,7 +51,7 @@ export default function ArtSection() {
 
             <div className="mt-8 text-center">
               <p className="font-serif italic text-sm opacity-50">
-                Tutti Frutti Women 73 (Inspired by a brave, bold and beautiful warrior)
+                Tutti Frutti Women 73 (Inspired by a bold, brave, and beautiful warrior)
               </p>
             </div>
           </div>
